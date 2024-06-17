dab56d24f3ca0b90a78c4e73c8353a057504e0b6
Marko A. Rodriguez <okrammarko@gmail.com>
2024-06-17 14:01:34 -0600

    worked on the CMake build file. No longer doing an in-source build. All compilation and artifacts are generated in a build/ directory for easy cleaning via rm -rf build/.

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,6 @@
 #include FOS_PROCESS(scheduler.hpp)
 #ifdef NATIVE
 #include FOS_MODULE(kernel/f_kernel.hpp)
-#include FOS_MODULE(io/f_log.hpp)
-#include FOS_MODULE(f_bcode.hpp)
 using namespace fhatos;
 
 using namespace fhatos;
@@ -51,20 +49,11 @@ int main(int arg, char **argsv) {
 };
 #else
 #include FOS_MODULE(kernel/f_kernel.hpp)
-#include FOS_MODULE(kernel/f_lang.hpp)
 #include FOS_MODULE(io/net/f_wifi.hpp)
-#include <structure/f_soc.hpp>
 #include FOS_PROCESS(thread.hpp)
 #include FOS_PROCESS(fiber.hpp)
 #include FOS_PROCESS(scheduler.hpp)
 #include FOS_ROUTER(local_router.hpp)
-#include FOS_ROUTER(mqtt_router.hpp)
-#include FOS_MODULE(io/f_log.hpp)
-#include <structure/f_soc.hpp>
-#include FOS_MODULE(io/f_serial.hpp)
-#include FOS_MODULE(io/net/f_ping.hpp)
-#include FOS_MODULE(io/net/f_telnet.hpp)
-#include FOS_MODULE(io/fs/f_fs.hpp)
 #include <language/fluent.hpp>
 #include <language/instructions.hpp>
 using namespace fhatos;
@@ -72,20 +61,7 @@ using namespace fhatos;
 void setup() {
   fKernel<>::bootloader({
       fWIFI::singleton(),
-      fKernel<>::singleton(),
-      FOS_DEFAULT_ROUTER::singleton(),
-      fScheduler<>::singleton(),
-      fMemory<>::singleton(),
-      fFS<>::singleton(),
-      fOTA<>::singleton(),
-      fLang<>::singleton()
   });
-  fScheduler<>::singleton()->spawn(fSoC<>::singleton());
-  fScheduler<>::singleton()->spawn(new fLog());
-  fScheduler<>::singleton()->spawn(fSerial<>::singleton());
-  fScheduler<>::singleton()->spawn(new fPing<>());
-  fScheduler<>::singleton()->spawn(fTelnet<>::singleton());
-
 }
 
 
