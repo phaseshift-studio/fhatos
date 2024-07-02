864587ba642acdf5f8eb661e78b89c38ad71f322
okram <148925+okram@users.noreply.github.com>
2024-07-02 20:15:11 +0000

    Deploying to gh-pages from @ phaseshift-studio/fhatos@edaef1ce96fbc3fbbc8e2f3604a52fe9781f06e2 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['localrouter_0',['LocalRouter',['../classfhatos_1_1LocalRouter.html',1,'fhatos']]],
-  ['lst_1',['LST',['../obj_8hpp.html#a48680c360c0948acdfc2bab032a0139ea8e07c292ed4718deaabff8e282a61953',1,'fhatos']]]
+  ['logger_1',['Logger',['../classfhatos_1_1Logger.html',1,'fhatos']]],
+  ['lst_2',['LST',['../obj_8hpp.html#a48680c360c0948acdfc2bab032a0139ea8e07c292ed4718deaabff8e282a61953',1,'fhatos']]]
 ];
--- search/classes_7.js
+++ search/classes_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['localrouter_0',['LocalRouter',['../classfhatos_1_1LocalRouter.html',1,'fhatos']]]
+  ['localrouter_0',['LocalRouter',['../classfhatos_1_1LocalRouter.html',1,'fhatos']]],
+  ['logger_1',['Logger',['../classfhatos_1_1Logger.html',1,'fhatos']]]
 ];
