698e082d2652320fa70ff1a37c514eeaaf7ee1d5
okram <148925+okram@users.noreply.github.com>
2024-07-05 19:22:37 +0000

    Deploying to gh-pages from @ phaseshift-studio/fhatos@e3093d6e151d10f92a327113343c92cc5183f6d3 🚀

--- search/classes_c.js
+++ search/classes_c.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['router_0',['Router',['../classfhatos_1_1Router.html',1,'fhatos']]],
-  ['router_3c_3e_1',['Router&lt;&gt;',['../classfhatos_1_1Router.html',1,'fhatos']]]
+  ['router_0',['Router',['../classfhatos_1_1Router.html',1,'fhatos']]]
 ];
