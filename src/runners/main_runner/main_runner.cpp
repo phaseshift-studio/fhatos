a1dd65d2e5731f6b8e479f67f1ba5418420ee967
okram <148925+okram@users.noreply.github.com>
2024-08-17 06:42:06 +0000

    Deploying to gh-pages from @ phaseshift-studio/fhatos@59c9ce438b982959a01e9033f249b1fa4dce4f3d 🚀

--- src/runners/main_runner/main_runner.cpp
+++ src/runners/main_runner/main_runner.cpp
@@ -6,10 +6,10 @@
 #include <language/insts.hpp>
 #include <language/parser.hpp>
 #include <language/types.hpp>
-#include <process/native/scheduler.hpp>
-#include <structure/console/console.hpp>
-#include <structure/io/terminal.hpp>
+#include <process/ptype/native/scheduler.hpp>
 #include <structure/kernel.hpp>
+#include <structure/model/console.hpp>
+#include <structure/model/terminal.hpp>
 #include <thread>
 #include <util/ansi.hpp>
 
@@ -23,34 +23,36 @@ void printResult(const Obj_p &obj, const uint8_t depth = 0) {
       printResult(o, depth + 1);
     }
   } else {
-    Options::singleton()->printer<>()->printf("!g==>!!%s\n", obj->toString().c_str());
+    printer<>()->printf("!g==>!!%s\n", obj->toString().c_str());
   }
 }
 
 int main(int arg, char **argsv) {
   try {
     Kernel::build()
-        ->with_printer(Ansi<>::singleton())
+        ->using_printer(Ansi<>::singleton())
         ->with_log_level(ERROR)
-        ->onBoot(Scheduler::singleton("/sys/scheduler/"), //
-                 {LocalRouter::singleton(), //
-                  Terminal::singleton("/sys/io/terminal/"), //
-                  Types::singleton("/sys/lang/type/"), //
-                  Parser::singleton("/sys/lang/parser/"), //
-                  new Console("/home/root/repl/")})
-        ->load_modules({"/ext/process"})
-        ->defaultOutput("/home/root/repl/"); // ->done("kernel_barrier");
-    Options::singleton()->printer<>()->on(false);
+        ->using_scheduler(Scheduler::singleton("/sys/scheduler/"))
+        ->using_router(Router::singleton("/sys/router/"))
+        ->boot<Terminal, Thread, KeyValue>(Terminal::singleton("/io/terminal/"))
+        ->boot<Types, Fiber, KeyValue>(Types::singleton("/type/"))
+        ->boot<Parser, Coroutine, Empty>(Parser::singleton("/sys/lang/parser/"))
+        ->boot<Console, Thread, Empty>(ptr<Console>(new Console("/home/root/repl/")))
+        //->boot<FileSystem, Fiber, Mount>(FileSystem::singleton("/io/fs"))
+        ->load_modules({ID("/mod/proc")})
+        ->initial_terminal_owner("/home/root/repl/");
+    //->done("kernel_barrier");
+    printer<>()->on(false);
   } catch (const std::exception &e) {
     throw;
   }
   LOG(INFO, "Processing %s\n", argsv[1]);
-  Options::singleton()->printer<>()->println("++++\n[source,mmadt]\n----");
+  printer<>()->println("++++\n[source,mmadt]\n----");
   for (int i = 1; i < arg; i++) {
     try {
       string x = argsv[i];
       StringHelper::trim(x);
-      Options::singleton()->printer<>()->printf("fhatos> %s\n", x.c_str());
+      printer<>()->printf("fhatos> %s\n", x.c_str());
       const Option<Obj_p> obj = Parser::singleton()->tryParseObj(argsv[i]);
       if (obj.has_value()) {
         printResult(Fluent(obj.value()).toObjs());
@@ -60,7 +62,7 @@ int main(int arg, char **argsv) {
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
-  Options::singleton()->printer<>()->print("----\n++++");
+  printer<>()->print("----\n++++");
   // Scheduler::singleton()->stop();
   return 0;
 }
