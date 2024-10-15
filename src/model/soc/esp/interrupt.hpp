d8414218ac7400659d6ce8312fc313b16ee6110a
Marko A. Rodriguez <okrammarko@gmail.com>
2024-10-15 06:57:21 -0600

    more tweaks to interrupt.

--- src/model/soc/esp/interrupt.hpp
+++ src/model/soc/esp/interrupt.hpp
@@ -31,37 +31,37 @@ namespace fhatos {
 
     static void ARDUINO_ISR_ATTR ISR_FUNCTION(void *arg) {
       const uint8_t pin = *(uint8_t *) arg;
-      if (Interrupt::singleton()->interrupts_.count(pin)) {
-        const BCode_p bcode = Interrupt::singleton()->interrupts_.at(pin);
-        Options::singleton()->processor<Obj,BCode,Obj>(noobj(),bcode);
+      if (Interrupt::singleton()->interrupts_->count(pin)) {
+        const BCode_p bcode = Interrupt::singleton()->interrupts_->at(pin);
+        Options::singleton()->processor<Obj, BCode, Obj>(noobj(), bcode);
       } else {
         LOG_STRUCTURE(ERROR, Interrupt::singleton(), "No interrupt code found for ISR_FUNCTION on pin %i", pin);
       }
     };
 
   protected:
-    Map<uint8_t, BCode_p> interrupts_;
+    Map_p<uint8_t, BCode_p> interrupts_;
     explicit Interrupt(const Pattern &pattern = "/soc/interrupt/#") :
         Pin(
             pattern,
             [this](const uint8_t pin) -> BCode_p {
-              return this->interrupts_.count(pin) ? this->interrupts_.at(pin) : noobj();
+              return this->interrupts_->count(pin) ? this->interrupts_->at(pin) : noobj();
             },
             [this](const uint8_t pin, const BCode_p &bcode) -> void {
-              if (this->interrupts_.count(pin)) {
-                this->interrupts_.erase(pin);
+              if (this->interrupts_->count(pin)) {
+                this->interrupts_->erase(pin);
                 detachInterrupt(pin);
                 LOG_STRUCTURE(INFO, this, "!bpin %i!! !yinterrupt!! detached\n", pin);
               }
               if (!bcode->is_noobj()) {
                 const BCode_p bclone = bcode->clone();
-                this->interrupts_.insert({pin, bclone});
+                this->interrupts_->insert({pin, bclone});
                 uint8_t *pin_ptr = new uint8_t(pin);
                 attachInterruptArg(pin, ISR_FUNCTION, (void *) pin_ptr, RISING);
                 LOG_STRUCTURE(INFO, this, "!bpin %i!! !yinterrupt!! attached\n", pin);
               }
             }),
-        interrupts_{{}} {}
+        interrupts_{make_shared<Map<uint8_t, BCode_p>>()} {}
 
   public:
     static ptr<Interrupt> singleton(const Pattern &pattern = "/soc/interrupt/#") {
