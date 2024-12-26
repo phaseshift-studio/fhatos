a23886b26d0a41f4b489d7cd7c1afb4bb643bb49
Marko A. Rodriguez <okrammarko@gmail.com>
2024-12-26 10:54:26 -0700

    using unique_ptr in fError to manage lifetime of char star message.

--- src/util/fhat_error.hpp
+++ src/util/fhat_error.hpp
@@ -33,39 +33,32 @@ namespace fhatos {
   ///////////////////////
   class fError : public std::exception {
   protected:
-    char _message[FOS_ERROR_MESSAGE_SIZE];
+    std::unique_ptr<const char*> message_;
 
-  public:
-    explicit fError(const char *format, ...) {
-      va_list arg;
-      va_start(arg, format);
-      int length = vsnprintf(_message, FOS_ERROR_MESSAGE_SIZE, format, arg);
-      _message[length] = '\0';
-      if (length >= FOS_ERROR_MESSAGE_SIZE) {
-        _message[FOS_ERROR_MESSAGE_SIZE - 1] = '\n';
-        _message[FOS_ERROR_MESSAGE_SIZE - 2] = '.';
-        _message[FOS_ERROR_MESSAGE_SIZE - 3] = '.';
-        _message[FOS_ERROR_MESSAGE_SIZE - 4] = '.';
-      }
-      va_end(arg);
+    template<typename... Args>
+    explicit fError(const std::string_view &format, const Args... args) : message_{
+      std::make_unique<const char*>(strdup(std::vformat(format, std::make_format_args(args...).c_str())))
     };
 
-    // ~fError() override { delete _message; }
+    template<typename... Args>
+    static fError create(const std::string &type_id, const std::string_view &format, const Args... args) noexcept {
+      return fError(string("!g[!m").append(type_id).append("!g] ").append(format), args...);
+    }
 
     [[nodiscard]] virtual const char *what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_USE_NOEXCEPT override {
-      return this->_message;
+      return *this->_message;
     }
     ////////////////////////////////
 #else
 
   class /*_LIBCPP_EXCEPTION_ABI _LIBCPP_AVAILABILITY_BAD_ANY_CAST*/ fError final : public std::exception {
   protected:
-    const char *_message;
+      std::unique_ptr<const char*> message_;
 
   public:
     template<typename... Args>
-    explicit fError(const std::string_view &format, const Args... args) noexcept: _message{
-      std::vformat(format, std::make_format_args(args...)).c_str()} {
+    explicit fError(const std::string_view &format, const Args... args) noexcept: message_{
+      std::make_unique<const char*>(strdup(std::vformat(format, std::make_format_args(args...)).c_str()))} {
     }
 
     template<typename... Args>
@@ -73,7 +66,7 @@ namespace fhatos {
       return fError(string("!g[!m").append(type_id).append("!g] ").append(format), args...);
     }
 
-    const char *what() const noexcept override { return this->_message; };
+    const char *what() const noexcept override { return *this->message_; };
 #endif
   };
 } // namespace fhatos
