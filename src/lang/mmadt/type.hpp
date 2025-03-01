bb0a0c3795c5f80ceb85fce9b241e0b204db00e1
Marko A. Rodriguez <okrammarko@gmail.com>
2025-03-01 03:18:01 -0700

    added code copy to clipboard -- smart to remove all console output. lots of nice css tweaks.

--- src/lang/mmadt/type.hpp
+++ src/lang/mmadt/type.hpp
@@ -78,12 +78,17 @@ namespace mmadt {
           ->domain_range(OBJ_FURI, {0, 1}, OBJ_FURI, {0, 1})
           ->type_args(x(0, "to_print", Obj::to_bcode()))
           ->inst_f([](const Obj_p &lhs, const InstArgs &args) {
-            auto line = args->arg(0)->str_value();
-            const string new_line = StringHelper::replace_groups(&line, "{", "}", [lhs](const string &match) {
-              const Obj_p result = OBJ_PARSER(match)->apply(lhs);
-              const string clean = Ansi<>::singleton()->strip(result->none_one_all()->toString());
-              return clean;
-            });
+            string new_line;
+            if(args->arg(0)->is_str()) {
+              auto line = args->arg(0)->str_value();
+              new_line = StringHelper::replace_groups(&line, "{", "}", [lhs](const string &match) {
+                const Obj_p result = OBJ_PARSER(match)->apply(lhs);
+                const string clean = result->none_one_all()->toString(NO_ANSI_PRINTER);
+                return clean;
+              });
+            } else {
+              new_line = args->arg(0)->toString(NO_ANSI_PRINTER);
+            }
             if(new_line.length() > 1 &&
                new_line[new_line.length() - 1] == 'n' &&
                new_line[new_line.length() - 2] == '\\')
