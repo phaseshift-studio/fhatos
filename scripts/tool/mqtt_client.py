41cfb7d7c3ec30703bacf2206eca01fbd6b09c0d
Marko A. Rodriguez <okrammarko@gmail.com>
2025-06-09 14:52:19 -0600

    more work on mqtt client llm tool.

--- scripts/tool/mqtt_client.py
+++ scripts/tool/mqtt_client.py
@@ -3,12 +3,12 @@ id: mqtt_client
 title: MQTT Client
 description: Enable an LLM to connect to an MQTT broker in order to publish messages and subscribe to topics.
 author: Dr. Stynx
-author_url: http:://markorodriguez.com
-funding_url: https://github.com/okram
+author_url: https://github.com/phaseshift-studio
+funding_url: https://github.com/sponsors/phaseshift-studio
 version: 0.0.1
 license: BSD 3.0
 """
-
+from paho.mqtt import MQTTException
 from pydantic import BaseModel, Field
 import paho.mqtt.client as mqtt
 import json
@@ -39,6 +39,7 @@ BOLD = '\u001b[1m'
 # CROSSED = "\u001b[9m"
 NC = '\u001b[0m'
 
+
 class MqttHelper:
     def on_connect(client: mqtt, userdata, flags, reason_code):
         print(f"Connected with result code {reason_code}")
@@ -54,83 +55,122 @@ class Tools:
         _VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX = "MQTT_CLIENT_VALVE_OVERRIDE_"
         MQTT_BROKER_URI: str = Field(
             default="localhost",
-            description=f"The uri of the mqtt broker; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_BROKER_URI.",
+            description=f"The uri of the mqtt broker; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_BROKER_URI."
         )
         MQTT_BROKER_PORT: int = Field(
             default=1883,
-            description=f"The port of the mqtt broker; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_BROKER_PORT.",
+            description=f"The port of the mqtt broker; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_BROKER_PORT."
         )
         MQTT_CLIENT: str = Field(
             default="llm_client",
-            description=f"The string name of the client connecting to the mqtt broker; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_CLIENT.",
+            description=f"The string name of the client connecting to the mqtt broker; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_CLIENT."
+        )
+        MQTT_CLIENT_LOG_LEVEL: str = Field(
+            default="INFO",
+            description=f"The string log level of the mqtt client; may be overridden by environment variable {_VALVE_OVERRIDE_ENVIRONMENT_VARIABLE_NAME_PREFIX}MQTT_CLIENT_LOG_LEVEL."
         )
 
     def __init__(self):
-        self.connected = False
         self.valves = self.Valves()
         self.citation = True
         self.client = mqtt.Client(
             client_id=self.valves.MQTT_CLIENT, protocol=mqtt.MQTTv311
         )
+        self.client.enable_logger()
+        self.client.logger.setLevel(self.valves.MQTT_CLIENT_LOG_LEVEL)
         self.client.on_connect = MqttHelper.on_connect
         self.client.on_message = MqttHelper.on_message
         pass
 
     #########################################################3
 
-    def connect(self) -> str:
+    def connect(self, client_id: str | None = None) -> str:
         """
         Connect to an MQTT broker
         """
-        if self.connected:
-            return "mqtt client already connected"
-        print(
-            "[{G}MQTT_CLIENT TOOL{NC}] model is connecting to broker:".format(G=GREEN,NC=NC),
+        if self.client.is_connected():
+            return json.dumps(
+                {
+                    "mqtt_broker": {"uri": self.valves.MQTT_BROKER_URI,
+                                    "port": self.valves.MQTT_BROKER_PORT},
+                    "mqtt_client": {"id": self.valves.MQTT_CLIENT if client_id is None else client_id,
+                                    "log_level": self.valves.MQTT_CLIENT_LOG_LEVEL},
+                    "result": "client already connected",
+                }
+            )
+        self.client.logger.info(
+            "[{G}MQTT_CLIENT TOOL{NC}] model is connecting to broker %s:%d".format(G=GREEN, NC=NC),
             self.valves.MQTT_BROKER_URI,
+            self.valves.MQTT_BROKER_PORT
         )
-        self.client.connect(
-            self.valves.MQTT_BROKER_URI, self.valves.MQTT_BROKER_PORT, 60
-        )
-        self.connected = True
+        if client_id is not None:
+            self.client = mqtt.Client(client_id=client_id)
+            self.client.enable_logger()
+            self.client.logger.setLevel(self.valves.MQTT_CLIENT_LOG_LEVEL)
+        result = self.client.connect(self.valves.MQTT_BROKER_URI, self.valves.MQTT_BROKER_PORT)
         self.client.loop_start()
-        return "mqtt client connected"
+        return json.dumps(
+            {
+                "mqtt_broker": {"uri": self.valves.MQTT_BROKER_URI,
+                                "port": self.valves.MQTT_BROKER_PORT},
+                "mqtt_client": {"id": self.valves.MQTT_CLIENT if client_id is None else client_id,
+                                "log_level": self.valves.MQTT_CLIENT_LOG_LEVEL},
+                "result": mqtt.error_string(result),
+            }
+        )
 
     def publish(self, topic: str, message: str, retain: bool = True) -> str:
         """
         Publish a message to a topic on the connected MQTT broker
         """
-        if not self.connected:
+        if not self.client.is_connected():
             self.connect()
-        print(
-            "[{G}MQTT_CLIENT TOOL{NC}] model is publishing".format(G=GREEN,NC=NC),
+        self.client.logger.info(
+            "[{G}MQTT_CLIENT TOOL{NC}] model is publishing %s to the topic %s at %s".format(G=GREEN, NC=NC),
             message,
-            "to the topic",
             topic,
-            "at",
             self.valves.MQTT_BROKER_URI,
         )
-        result = self.client.publish(topic=topic, payload=message, retain=retain)
-        return json.dumps(
-            {
-                "mqtt_broker": self.valves.MQTT_BROKER_URI,
-                "topic": topic,
-                "message": message,
-                "retain": retain,
-                #  "result": result,
-            }
-        )
+        try:
+            result = self.client.publish(topic=topic, payload=message, retain=retain)
+            result.wait_for_publish(5.0)  # wait for 5 seconds on publish
+            return json.dumps(
+                {
+                    "mqtt_broker": self.valves.MQTT_BROKER_URI,
+                    "message": {
+                        "message_id": result.mid,
+                        "topic": topic,
+                        "message": message,
+                        "retain": retain,
+                        "message_published": result.is_published(),
+                    },
+                    "result": mqtt.error_string(result.rc)
+                }
+            )
+        except MQTTException as e:
+            return json.dumps(
+                {
+                    "mqtt_broker": self.valves.MQTT_BROKER_URI,
+                    "message": {
+                        "topic": topic,
+                        "message": message,
+                        "retain": retain,
+                        "message_published": False
+                    },
+                    "result": str(e)
+                }
+            )
 
     def subscribe(self, topic: str) -> str:
         """
         Subscribe to a topic on the connected MQTT broker
         """
-        if not self.connected:
+        if not self.client.is_connected():
             self.connect()
-        print(
-            "[{G}MQTT_CLIENT TOOL{NC}] model is subscribing to the topic".format(G=GREEN,NC=NC),
+        self.client.logger.info(
+            "[{G}MQTT_CLIENT TOOL{NC}] model is subscribing to the topic %s at %s".format(G=GREEN, NC=NC),
             topic,
-            "at",
-            self.valves.MQTT_BROKER_URI,
+            self.valves.MQTT_BROKER_URI
         )
         result = self.client.subscribe(topic)
         return json.dumps(
